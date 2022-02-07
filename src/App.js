3ce743306e2f969cb2ed63a533e783c921dfd334
cvegah <cesar.vega.hdez@gmail.com>
2022-02-07 17:34:47 -0600

    Tasks.json
    tasks.json file is created
    file is imported
    json is looped with map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import tasks from "./sample/tasks.json";
+
 class App extends Component {
+
+  state = {
+    tasks: tasks
+  }
+
   render () {
-    return <h1>Hello hell</h1>
+    return <div>
+      {
+        this.state.tasks.map(e => 
+          <p key={e.id}>
+            {e.name} - {e.description}
+          </p>
+        )
+      }
+    </div>
   }
 }
 
