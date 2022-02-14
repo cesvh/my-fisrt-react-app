f5aae4f77e22459b4919a88d1599c2baa77ca481
cvegah <cesar.vega.hdez@gmail.com>
2022-02-13 19:06:45 -0600

    Projects in components
    Component Task and Tasks

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 import tasks from "./sample/tasks.json";
 
+import Tasks from './components/Tasks';
+
 class App extends Component {
 
   state = {
@@ -11,13 +13,7 @@ class App extends Component {
 
   render () {
     return <div>
-      {
-        this.state.tasks.map(e => 
-          <p key={e.id}>
-            {e.name} - {e.description}
-          </p>
-        )
-      }
+      <Tasks tasks={this.state.tasks} />
     </div>
   }
 }
