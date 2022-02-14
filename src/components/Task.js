02ac799329635f2795556b9d309a46c3ab48930c
cvegah <cesar.vega.hdez@gmail.com>
2022-02-13 20:26:17 -0600

    Estilos en linea y archivo externo

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,17 +1,33 @@
 import React, { Component } from "react";
 
+import "./Task.css";
+
 class Task extends Component {
     render() {
         const {task} = this.props;
-        return <div>
+        // const redColor = { background: 'red'};
+        return <p 
+        /*style={redColor}*/
+        /*className="back"*/ 
+        /*style={{ background: 'red' }}*/>
             { task.name } - 
             { task.description }
             <input type="checkbox" />
-            <button>
+            <button style={btnDelete}>
                 x
             </button>
-        </div>
+        </p>
     }
 }
+
+const btnDelete = {
+    fontSize: '18px',
+    background: '#ea2027',
+    color: '#fff',
+    border: 'none',
+    padding: '10px 15px',
+    borderRadius: '50%',
+    cursor: 'pointer'
+};
 
 export default Task;
